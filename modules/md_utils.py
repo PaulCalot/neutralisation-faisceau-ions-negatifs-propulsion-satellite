34f18c5cac90e4d1f8f7e764851c1a9066a6669f
PaulCalot <paul.calot@hotmail.fr>
2021-04-01 14:13:01 +0200

    Realized there is an error with something; missing files ?

--- modules/md_utils.py
+++ modules/md_utils.py
@@ -2,33 +2,43 @@ import numpy as np
 import os
 
 
-def coord_initiales_ion(chemin):
-    file = open(chemin, "r")
+def coord_initiales_ion(path):
+    lenght_headers = longueur_file(path)
+    file = open(path, "r")
     
-    test=True
-    while test: #ou 8 ?
+    found = False
+
+    while not found:
         line = file.readline()
-        extrait=line[10:13]
-        if extrait=="COM":
-            test=False
-    file.close()
-    indice_2_points=0
-    indice_1er_espace=0
-    indice_2eme_espace=0
-    for i in range(len(line)):
-        if indice_2_points==0 and line[i]==':':
-            indice_2_points=i
-        elif indice_1er_espace==0 and indice_2_points!=0 and i>indice_2_points+1 and line[i]==' ':
-            indice_1er_espace=i
-        elif indice_2eme_espace==0 and indice_1er_espace!=0 and line[i]==' ':
-            print(i)
-            indice_2eme_espace=i
-            
-    xini=float(line[indice_2_points+2: indice_1er_espace])
-    yini=float(line[indice_1er_espace+1: indice_2eme_espace])
-    zini=float(line[indice_2eme_espace+1:])
+
+        if(line == ""): # https://docs.python.org/3.6/tutorial/inputoutput.html#methods-of-file-objects
+            print('End of file reached. Cound not find initial ion coordinates.')
+            return
     
-    return xini,yini,zini
+        split = line.split(':')
+        if split[0]=="# Initial COM position":
+            file.close()
+            found = True
+            xini,yini,zini = split[1].strip().split(' ')
+            return float(xini), float(yini), float(zini)
+
+    # indice_2_points=0
+    # indice_1er_espace=0
+    # indice_2eme_espace=0
+    # for i in range(len(line)):
+    #     if indice_2_points==0 and line[i]==':':
+    #         indice_2_points=i
+    #     elif indice_1er_espace==0 and indice_2_points!=0 and i>indice_2_points+1 and line[i]==' ':
+    #         indice_1er_espace=i
+    #     elif indice_2eme_espace==0 and indice_1er_espace!=0 and line[i]==' ':
+    #         print(i)
+    #         indice_2eme_espace=i
+            
+    # xini=float(line[indice_2_points+2: indice_1er_espace])
+    # yini=float(line[indice_1er_espace+1: indice_2eme_espace])
+    # zini=float(line[indice_2eme_espace+1:])
+    
+    # return xini,yini,zini
 
 def complementaire(i):
     if i+1<10:
@@ -54,8 +64,8 @@ def longueur_intro(chemin):
     file = open(chemin, "r")
     ind=0
     line = file.readline()
-    while line[0]!="0":
+    while line[0]=="#" or line[0]=="%":
         line = file.readline()
         ind+=1
     file.close()
-    return ind-1
+    return ind
