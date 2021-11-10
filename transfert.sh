1b32305e1cb857fefb0cf317efdf74d4da28c853
PaulCalot <paul.calot@hotmail.fr>
2021-11-10 21:07:48 +0100

    Cleaned branch

--- transfert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp "/Users/edouardroger/Documents/Cours/X/Projet spatial/Neutralisation_Faisceau_ions_negatifs_propulsion_satellite/"* edouard.roger@truite.polytechnique.fr:~/projet/
-scp "/Users/edouardroger/Documents/Cours/X/Projet spatial/Neutralisation_Faisceau_ions_negatifs_propulsion_satellite/modules/"* edouard.roger@truite.polytechnique.fr:~/projet/modules/
-
